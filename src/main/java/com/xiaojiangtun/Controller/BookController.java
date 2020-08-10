861f523fbe2e38965062d69d2f742dfa6268b4bd
fudayu0527 <fudayu0527@163.com>
2020-08-10 17:46:45 +0800

    springboot rabbitmq TTL延时消息

--- src/main/java/com/xiaojiangtun/Controller/BookController.java
+++ src/main/java/com/xiaojiangtun/Controller/BookController.java
@@ -2,11 +2,16 @@ package com.xiaojiangtun.Controller;
 
 import com.xiaojiangtun.config.RabbitConfig;
 import com.xiaojiangtun.entity.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.AbstractJavaTypeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * rabbitmq
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/books")
 public class BookController {
+
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
     private final RabbitTemplate rabbitTemplate;
 
@@ -26,13 +33,32 @@ public class BookController {
      * this.rabbitTemplate.convertAndSend(RabbitConfig.DEFAULT_BOOK_QUEUE, book); 对应 { BookHandler#listenerAutoAck}
      * this.rabbitTemplate.convertAndSend(RabbitConfig.MANUAL_BOOK_QUEUE, book); 对应 {  BookHandler#listenerManualAck}
      */
-    @GetMapping
+    @GetMapping("/defaultMessage")
     public void defaultMessage() {
         Book book = new Book();
         book.setId("1");
         book.setName("一起来学Spring Boot");
         this.rabbitTemplate.convertAndSend(RabbitConfig.DEFAULT_BOOK_QUEUE, book);
         this.rabbitTemplate.convertAndSend(RabbitConfig.MANUAL_BOOK_QUEUE, book);
+    }
+
+    /**
+     * this.rabbitTemplate.convertAndSend(RabbitConfig.REGISTER_DELAY_EXCHANGE, RabbitConfig.DELAY_ROUTING_KEY, book); 对应 { BookHandler#listenerDelayQueue}
+     */
+    @GetMapping("/defaultMessageTTL")
+    public void defaultMessageTTL() {
+        Book book = new Book();
+        book.setId("1");
+        book.setName("一起来学Spring Boot");
+        // 添加延时队列
+        this.rabbitTemplate.convertAndSend(RabbitConfig.REGISTER_DELAY_EXCHANGE, RabbitConfig.DELAY_ROUTING_KEY, book, message -> {
+            // TODO 第一句是可要可不要,根据自己需要自行处理
+            message.getMessageProperties().setHeader(AbstractJavaTypeMapper.DEFAULT_CONTENT_CLASSID_FIELD_NAME, Book.class.getName());
+            // TODO 如果配置了 params.put("x-message-ttl", 5 * 1000); 那么这一句也可以省略,具体根据业务需要是声明 Queue 的时候就指定好延迟时间还是在发送自己控制时间
+            message.getMessageProperties().setExpiration(5 * 1000 + "");
+            return message;
+        });
+        log.info("[发送时间] - [{}]", LocalDateTime.now());
     }
 
 
