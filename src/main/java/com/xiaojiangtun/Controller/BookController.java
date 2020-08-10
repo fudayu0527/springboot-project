57dfc88ec59937a0a192c3a9dc3e96cb39f77400
fudayu0527 <fudayu0527@163.com>
2020-08-10 17:37:24 +0800

    springboot 集成rabbitmq接口文档

---
+++ src/main/java/com/xiaojiangtun/Controller/BookController.java
@@ -0,0 +1,39 @@
+package com.xiaojiangtun.Controller;
+
+import com.xiaojiangtun.config.RabbitConfig;
+import com.xiaojiangtun.entity.Book;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * rabbitmq
+ */
+@RestController
+@RequestMapping(value = "/books")
+public class BookController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public BookController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    /**
+     * this.rabbitTemplate.convertAndSend(RabbitConfig.DEFAULT_BOOK_QUEUE, book); 对应 { BookHandler#listenerAutoAck}
+     * this.rabbitTemplate.convertAndSend(RabbitConfig.MANUAL_BOOK_QUEUE, book); 对应 {  BookHandler#listenerManualAck}
+     */
+    @GetMapping
+    public void defaultMessage() {
+        Book book = new Book();
+        book.setId("1");
+        book.setName("一起来学Spring Boot");
+        this.rabbitTemplate.convertAndSend(RabbitConfig.DEFAULT_BOOK_QUEUE, book);
+        this.rabbitTemplate.convertAndSend(RabbitConfig.MANUAL_BOOK_QUEUE, book);
+    }
+
+
+}
