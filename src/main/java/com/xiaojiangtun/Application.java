bdb78a2df6a4f86d8c35f2c258e6c6ca45a112a7
fudayu0527 <fudayu0527@163.com>
2020-08-11 11:17:25 +0800

    springboot 定时任务

--- src/main/java/com/xiaojiangtun/Application.java
+++ src/main/java/com/xiaojiangtun/Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +24,25 @@ import java.util.Arrays;
  */
 //@RestController
 @EnableSwagger2Doc
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * 很关键：默认情况下 TaskScheduler 的 poolSize = 1
+     *
+     * @return 线程池
+     */
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(10);
+        return taskScheduler;
     }
 
 //    @GetMapping("/demo1")
